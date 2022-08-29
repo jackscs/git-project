005e69e67d9cf59f7f34eda1ce37483af0e52d6d
jackscs <112260485+jackscs@users.noreply.github.com>
2022-08-29 16:25:49 +0800

    99乘法表

--- hello.go
+++ hello.go
@@ -7,6 +7,14 @@ import(
 func main(){
     fmt.Println("master second")
     fmt.Println("hot-fix 33333")
-    fmt.Println("master")
+    fmt.Println("master") 
     fmt.Println("hot-fix 2222")
+    fmt.Println("通过远程进行代码修改")
+    //乘法表
+    for i:=1;i<9;i++{
+        for j:=1;j<=i;j++{
+            fmt.Printf("%d  ",i*j)
+        }
+        fmt.Println()
+    }
 }
